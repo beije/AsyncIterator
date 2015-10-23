9ca8dd74caed6e4d20ab6ff290b4f71710e68397
beije <git@benjaminhorn.se>
2015-10-24 00:29:56 +0300

    Initial commit

---
+++ AsyncIterator.js
@@ -0,0 +1,65 @@
+var asyncIterator = (function($) {
+	/**
+	 * Runs a callback on a large dataset without holding up the main thread.
+	 *
+	 * @param {array}		arr 		The dataset
+	 * @param {function}	cb 			The callback function
+	 * @param {int}			batchSize	(Optional) the number of operations per call, default: 10
+	 */
+	var AsyncIterator = function(arr, cb, batchSize) {
+		this.arr = arr;
+		this.cb = cb;
+		this.batchSize = parseInt(batchSize) || 10;
+		this.position = 0;
+		this.results = [];
+
+		this.timer = null;
+
+		this._deferred;
+	}
+
+	/**
+	 * Starts the iteration of the dataset
+	 *
+	 * @return {Jquery deferred}	A promise that will resolve when operations are done.
+	 */
+	AsyncIterator.prototype.process = function() {
+		this._deferred = new $.Deferred();
+
+		this._iterate();
+
+		return this._deferred;
+	}
+
+	/**
+	 * The actual operating part
+	 * @private
+	 */
+	AsyncIterator.prototype._iterate = function() {
+		for(var i = 0; i  < this.batchSize; i++) {
+			if(this.position >= this.arr.length) { break; }
+
+			var currentItem = this.arr[this.position];
+
+			this.results[this.position] = this.cb(currentItem, this.position);
+
+			this.position++;
+		}
+
+		if(this.position < this.arr.length) {
+			window.requestAnimationFrame(this._iterate.bind(this));
+		} else {
+			this._deferred.resolve(this.results);
+		}
+	}
+
+	/**
+	 * The public method for creating the AsyncIterator
+	 *
+	 * @return {Jquery deferred}	A promise that will resolve when operations are done.
+	 */
+	return function(arr, cb, size) {
+		var it = new AsyncIterator(arr, cb, size);
+		return it.process();
+	}
+})(jQuery);
---
+++ example/App.js
@@ -0,0 +1,69 @@
+(function(){
+
+    // Create a dataset with n amount of items
+    var myArr = [];
+    for(var i = 0; i < 500000; i++) {
+        myArr.push(i)
+    }
+    
+    $('#run-for').on(
+        'click',
+        function() {
+            var startTime = new Date();
+            var res;
+            for(var i = 0; i < myArr.length; i++) {
+                res = myArr[i] * Math.PI;
+                $('#for-status').html(Math.round((i/myArr.length)*100) + '%');
+            }
+            $('#for-time').html(new Date() - startTime);
+        }
+    );
+    
+    $('#run-lift').on(
+        'click',
+        function() {
+            var startTime = new Date();
+            
+            // Initiate the iterator
+            var promise = asyncIterator(
+                myArr,
+                function(num, index){
+                    $('#async-lift-status').html(Math.round((index/myArr.length)*100) + '%');
+                    return num * Math.PI;
+                }, 
+                1
+            );
+            
+            // Console log the results when it's done
+            promise.done(
+                function(results){
+                    var endTime = new Date();
+                    $('#async-lift-time').html(new Date() - startTime);
+                }
+            );
+        }
+    );
+    
+    $('#run-batched').on(
+        'click',
+        function() {
+            var startTime = new Date();
+            // Initiate the iterator
+            var promise = asyncIterator(
+                myArr,
+                function(num, index){
+                    $('#async-batched-lift-status').html(Math.round((index/myArr.length)*100) + '%');
+                    
+                    return num * Math.PI;
+                }, 
+                1000
+            );
+            
+            promise.done(
+                function(results){
+                    $('#async-batched-lift-time').html(new Date() - startTime);
+                }
+            );
+        }
+    );
+})();
